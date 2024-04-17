9371321e37adabe188ccd2102f9153b2fe5285ac
Mayank07-HP <mayank07.hp@gmail.com>
2024-04-17 17:41:09 +0530

    fix errors

--- src/ProtectedRoute/ProtectedRoute.tsx
+++ src/ProtectedRoute/ProtectedRoute.tsx
@@ -1,29 +1,20 @@
 
 import { AuthContext } from "@/context/auth-context";
 import { useRouter } from "next/navigation";
-import  { ReactNode, useContext, useEffect, useState } from "react";
+import { ReactNode, useContext, useEffect, useState } from "react";
 
 export const ProtectedRoute = ({ children }: { children: ReactNode }) => {
-    const [session,setSession]=useState(false)
-    const { currentUser } = useContext(AuthContext)
-    const router = useRouter()
-    
-    useEffect(() => {
-     const  session =sessionStorage.getItem("user")
-     if(session){
-      setSession(true)
-     }else{
-      setSession(false)
-     }
+  const { currentUser } = useContext(AuthContext)
+  const router = useRouter()
+
+  useEffect(() => {
+    const session = sessionStorage.getItem("user")
+    if (!session) {
+      router.push("Login");
+    }
   }, []);
 
   const isAuthenticated = currentUser
-  console.log(session)
+  return <>{children}</>;
 
-  if (isAuthenticated||session) {
-    return <>{children}</>;
-  } else {
-    router.push("Login");
-    return null;
-  }
-};
+}
