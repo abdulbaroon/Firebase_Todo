e5876af15870f025170cbd44b9c69283ad65b52f
Mayank07-HP <mayank07.hp@gmail.com>
2024-04-17 16:42:25 +0530

    git ignore

--- src/utils/AllUserData.tsx
+++ src/utils/AllUserData.tsx
@@ -3,19 +3,17 @@ import { getDatabase, onValue, ref } from "firebase/database";
 import { useEffect, useState } from "react";
 
 const allUsersData=()=>{
-  const [allUsers, setAllUsers] = useState<any[]>([])
   const db = getDatabase(app);
+  const newUsers:any[]= [];
  useEffect(() => { 
         const userRef = ref(db, `/users`);
         onValue(userRef, (snapshot) => {
             const users = snapshot.val();
-            const newUsers:any[]= [];
             for (let id in users) {
               newUsers.push({ id, ...users[id] });
             }
-            setAllUsers(newUsers);
         });
   }, [db]);
-  return allUsers
+  return newUsers
 }
 export {allUsersData}
